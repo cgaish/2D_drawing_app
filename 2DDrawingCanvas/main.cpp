c9e9137dad56a95a60d13b5ffa420a83772bf39b
cgaish <cgaishwarya@gmail.com>
2021-05-21 13:37:43 +0530

    Implemented round point drawings using fragment shader, QSurfaceformat setup, OpenGL debug callback setup

--- 2DDrawingCanvas/main.cpp
+++ 2DDrawingCanvas/main.cpp
@@ -1,10 +1,21 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSurfaceFormat>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Global format setup
+    QSurfaceFormat canvasFormat(QSurfaceFormat::DebugContext);
+    canvasFormat.setDepthBufferSize(24);
+    canvasFormat.setStencilBufferSize(8);
+    canvasFormat.setVersion(4, 6);
+    canvasFormat.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(canvasFormat);
+
+    // Init objects
     MainWindow w;
     w.show();
     return a.exec();
